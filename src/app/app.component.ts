9bcff7aff426bb4961229e18e333d073cd460db0
Oneshot369 <joshcpeck4@gmail.com>
2023-10-03 11:44:59 -0700

    modified test app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testapp';
+  title = 'Different title of app';
+  message = 'Josh';
 }
